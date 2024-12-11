36d2735ad0eef9737e8d084d95a606092f779524
Enuguru Akshay <akshayenuguru3@gmail.com>
2024-12-11 19:24:21 +0530

    Time: 1 ms (99.38%), Space: 57.4 MB (10.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentmax = nums[0];
+        int globalmax = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            currentmax = Math.max(nums[i],currentmax+nums[i]);
+            if(currentmax>globalmax){
+                globalmax = currentmax;
+            }
+        }
+        return globalmax;
+    }
+}
