97252aa40ba4f53c7002e88a34f9b6245b62f820
Enuguru Akshay <akshayenuguru3@gmail.com>
2024-12-12 10:25:41 +0530

    Time: 6 ms (65.07%), Space: 42.3 MB (76.48%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,12 +1,19 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        for (int i = 0; i < k; i++) {
-            Arrays.sort(gifts);
-            gifts[gifts.length - 1] = (int) Math.sqrt(gifts[gifts.length - 1]);
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        for(int gift:gifts){
+            q.offer(gift);
+        }
+        for(int i=0;i<k;i++){
+            if(!q.isEmpty()){
+                int largest = q.poll();
+                int reduced = (int)Math.sqrt(largest);
+                q.offer(reduced);
+            }
         }
         long sum = 0;
-        for (int gift : gifts) {
-            sum += gift;
+        while(!q.isEmpty()){
+            sum+=q.poll();
         }
         return sum;
     }
