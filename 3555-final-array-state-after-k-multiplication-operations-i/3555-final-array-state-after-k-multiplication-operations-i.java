fd92e0cd09afd2d4cfdb024182b9f754384773a6
Enuguru Akshay <akshayenuguru3@gmail.com>
2024-12-16 20:02:47 +0530

    Time: 1 ms (100%), Space: 44.9 MB (34.76%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0]; 
-            return a[1] - b[1]; 
-        });
-        for(int i=0;i<nums.length;i++){
-            q.offer(new int[]{nums[i],i});
-        }
+        int[] arr = new int[2];
         for(int i=0;i<k;i++){
-            int[] picking = q.poll();
-            int index = picking[1];
-            nums[index] = nums[index]*multiplier;
-            q.offer(new int[]{nums[index],index});
+            arr = helper(nums); 
+            nums[arr[1]] = arr[0]*multiplier;
         }
-        /*while(!q.isEmpty()){
-            int[] picking = q.poll();
-            int num = picking[0];
-            int index = picking[1];
-            nums[index] = num;
-        }*/
         return nums;
+    }
+    static int[] helper(int[] nums){
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+                index = i;
+            }
+        }
+        return new int[]{min,index};
     }
 }
