6dd3d13be5e188c51f8c7b20d76bed1eabc35385
Enuguru Akshay <akshayenuguru3@gmail.com>
2025-03-03 20:36:06 +0530

    Time: 2 ms (99.74%), Space: 46.6 MB (93.71%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        Queue<Integer> p = new LinkedList<>();
+        /*Queue<Integer> p = new LinkedList<>();
         Queue<Integer> n = new LinkedList<>();
         int[] ans = new int[nums.length];
         for(int num:nums){
@@ -15,6 +15,19 @@ class Solution {
                 ans[i] = p.poll();
             }else{
                 ans[i] = n.poll();
+            }
+        }
+        return ans;*/
+        int[] ans = new int[nums.length];
+        int ei = 0;
+        int ni = 1;
+        for(int num : nums){
+            if(num%2==0){
+                ans[ei] = num;
+                ei+=2;
+            }else{
+                ans[ni] = num;
+                ni+=2;
             }
         }
         return ans;
