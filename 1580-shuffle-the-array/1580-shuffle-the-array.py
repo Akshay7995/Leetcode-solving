82238dc53929bc83a76dc7730e20ecd321d63dc0
Enuguru Akshay <akshayenuguru3@gmail.com>
2025-04-16 16:39:31 +0530

    Time: 56 ms (73.3%), Space: 17.9 MB (61.96%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,12 +1,25 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        ans = [ ]
+        """ans = [ ]
         i=0
         j=len(nums)//2
         while(j<len(nums)):
             ans.append(nums[i])
             ans.append(nums[j])
             i+=1
+            j+=1
+        
+        return ans"""
+        ans = [0]*len(nums)
+        i=0
+        j=len(nums)//2
+        pointer = 0
+        while(j<len(nums)):
+            ans[pointer] = nums[i]
+            i+=1
+            pointer+=1
+            ans[pointer]=nums[j]
+            pointer+=1
             j+=1
         
         return ans
