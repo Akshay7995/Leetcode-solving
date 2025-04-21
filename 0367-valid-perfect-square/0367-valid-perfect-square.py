4d9a5f47e3b2aa471c89f55efd645b08aa953a97
Enuguru Akshay <akshayenuguru3@gmail.com>
2025-04-21 16:41:11 +0530

    Time: 0 ms (100%), Space: 17.6 MB (96.22%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -2,8 +2,12 @@ class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         if num<0:
             return False
-        number = int(num**0.5)+1
+        """number = int(num**0.5)+1
         for i in range(1,number):
             if i*i == num:
                 return True
+        return False"""
+        number = int(math.sqrt(num))
+        if number*number == num:
+            return True
         return False
